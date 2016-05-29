4eb71e90ec305a532c9b8844e4a8ae6d9dea4070
Zhorin Sergey <ser4038772@mail.ru>
2016-05-30 00:46:07 +0300

    39

--- solutions/Dijkstra/Sample/main.cpp
+++ solutions/Dijkstra/Sample/main.cpp
@@ -5,6 +5,7 @@
 #include "Heap.h"
 #include <limits.h>
 #include <iostream>
+#include "tree.h"
 
 void main(int argc, char *argv[]) {
   int N, M;
@@ -58,7 +59,44 @@ void main(int argc, char *argv[]) {
         och.update(i->first, dist[j] + i->second);
       }
       if (((dist[j] + i->second) == dist[i->first])
-      && ((dist1[j] + 1) > dist1[i->first])) {
+        && ((dist1[j] + 1) > dist1[i->first])) {
+        up[i->first] = j;
+        dist1[i->first] = dist1[j] + 1;
+      }
+    }
+  }
+  std::cout << dist[N] << " " << dist1[N] << std::endl;
+  avltree *tree = NULL;
+  dist[1] = 0;
+  dist1[1] = 0;
+  up[1] = 0;
+  tree = avltree_add(tree, 0, 1);
+  for (int k = 2; k < N + 1; k++) {
+    up[k] = 0;
+    dist[k] = INT_MAX;
+    dist1[k] = 0;
+    tree = avltree_add(tree, INT_MAX, k);
+  }
+  int j = get_value(tree);
+  while (tree != 0) {
+    avltree *tmp = 0;
+    tree = avltree_min(tree, &tmp);
+    int j = get_value(tmp);
+    for (auto i = graph[j].begin(); i != graph[j].end(); ++i) {
+
+      if ((dist[j] + i->second) < dist[i->first]) {
+        if (tree == 0)
+          continue;
+        int tmp1 = dist[i->first];
+        dist[i->first] = dist[j] + i->second;
+        up[i->first] = j;
+        dist1[i->first] = dist1[j] + 1;
+        tree = avltree_del(tree, i->first, tmp1);
+        tree = avltree_add(tree, dist[j] + i->second, i->first);
+      }
+
+      if (((dist[j] + i->second) == dist[i->first])
+        && ((dist1[j] + 1) > dist1[i->first])) {
         up[i->first] = j;
         dist1[i->first] = dist1[j] + 1;
       }
